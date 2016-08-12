e626cbdb490e6f5eb9b77597a06d45a7488726f6
dmitry <dmitry.kanishchev@gmail.com>
2016-08-12 14:59:49 +0300

    Реализована фильтрация

--- src/main/java/com/moracle/webticketsystem/model/service/TicketService.java
+++ src/main/java/com/moracle/webticketsystem/model/service/TicketService.java
@@ -1,6 +1,8 @@
 package com.moracle.webticketsystem.model.service;
 
+import com.moracle.webticketsystem.model.entity.Priority;
 import com.moracle.webticketsystem.model.entity.Project;
+import com.moracle.webticketsystem.model.entity.Status;
 import com.moracle.webticketsystem.model.entity.Ticket;
 
 import java.util.List;
@@ -16,6 +18,8 @@ public interface TicketService {
     Ticket getById(int id);
 
     List<Ticket> getByProject(Project project, int page, int size);
+
+    List getByProjectWithFilter(Project project, Status status, Priority priority);
 
     int countByProject(Project project);
 }
